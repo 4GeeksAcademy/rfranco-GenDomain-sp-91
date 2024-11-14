90e855c1a8c1371a350d8ce5b5e2af9e5c55161f
Ricardo Franco Pérez <rfrancop01@gmail.com>
2024-11-14 20:33:10 +0000

    [sp-91] Generador de dominios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronun = ["El", "Tu", "Nuestro"];
+let adj = ["gran", "pequeño"];
+let noun = ["perro", "hamster", "tigre", "delfín"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let a = 0; a < pronun.length; a++) {
+  for (let b = 0; b <  adj.length; b++) {
+    for (let c = 0; c < noun.length; c++) {
+      console.log(pronun[a] + adj[b] + noun[c] + ".com");
+    }
+  }
+}
